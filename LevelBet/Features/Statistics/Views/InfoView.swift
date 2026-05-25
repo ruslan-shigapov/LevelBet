a99d1b2bc3dd22b7b423b775669ee88aac4e3573
Ruslan Shigapov <ilgamovich@gmail.com>
2026-05-25 12:56:33 +0300

    Setup InfoView

--- LevelBet/Features/Statistics/Views/InfoView.swift
+++ LevelBet/Features/Statistics/Views/InfoView.swift
@@ -8,44 +8,66 @@
 import SwiftUI
 
 struct InfoView: View {
-    
-    @Environment(\.dismiss) private var dismiss
-    
-    @ToolbarContentBuilder
-    private var toolbarContent: some ToolbarContent {
-        ToolbarItem {
-            ToolbarButton(type: .cancel) {
-                dismiss()
-            }
-        }
-    }
-    
+        
     var body: some View {
         NavigationStack {
             ScrollView {
-                InfoCard(title: "Профит", text: "Чистая прибыль")
+                InfoCard(
+                    title: "Профит",
+                    text: """
+                    Показывает чистую прибыль за выбранный период.
+                    """)
+                InfoCard(
+                    title: "ROI",
+                    text: """
+                    Показывает окупаемость относительно вложенных средств.
+                    
+                    Даже небольшое положительное значение на длинной дистанции считается хорошим результатом.
+                    """)
+                InfoCard(
+                    title: "Винрейт по купонам",
+                    text: """
+                    Показывает процент выигранных купонов от их общего количества.
+                    
+                    Даже при высоком винрейте профит может быть отрицательным, если коэффициенты слишком низкие.
+                    """)
+                InfoCard(
+                    title: "Винрейт по событиям",
+                    text: """
+                    Показывает процент выигранных событий внутри всех купонов. Помогает оценить качество выбора событий.
+                    
+                    Высокий показатель при слабом результате может указывать на слишком большое количество событий в купонах.
+                    """)
+                InfoCard(
+                    title: "Детальный ROI и винрейт",
+                    text: """
+                    Разбивка показателей по количеству событий, диапазону коэффициентов или виду спорта помогает увидеть слабые и сильные стороны стратегии.
+                    """)
+                InfoCard(
+                    title: "Средние показатели",
+                    text: """
+                    Показывают общий стиль игры на дистанции. Сравнение значений для выигранных и проигранных купонов помагают оценить закономерности.
+                    """)
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding()
             .background(Color.midnight)
             .navigationTitle("Справка")
             .navigationBarTitleDisplayMode(.inline)
-            .toolbar { toolbarContent }
         }
         .presentationDragIndicator(.visible)
     }
     
     private func InfoCard(title: String, text: String) -> some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: Layouts.mediumOffset) {
             Text(title)
-                .font(.headline)
+                .font(.body.weight(.medium))
             Text(text)
-                .font(.body)
+                .font(.body.weight(.light).italic())
         }
         .frame(maxWidth: .infinity, alignment: .leading)
         .padding()
+        .background(Color(uiColor: .systemGroupedBackground))
+        .clipShape(RoundedRectangle(cornerRadius: 25))
     }
-}
-
-#Preview {
-    InfoView()
 }
